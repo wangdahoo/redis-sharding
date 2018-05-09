864ffe15767794afd894f2dd3f9deef2f0c08467
wangdahoo <157195705@qq.com>
2018-05-09 16:53:20 +0800

    v0.2.0

--- README.md
+++ README.md
@@ -7,6 +7,8 @@
 $ npm i redis-sharding
 ```
 
+* client
+
 ```js
 const Sharding = require('redis-sharding')
 
@@ -23,4 +25,25 @@ client
   .then(server => console.log(server)) // => 127.0.0.1:7001
   .then(() => client.get('foo'))
   .then(value => console.log(value)) // => bar
+```
+
+* multi
+
+```js
+const Sharding = require('redis-sharding')
+
+const sharding = new Sharding({
+  '127.0.0.1:7000': { vnodes: 50 },
+  '127.0.0.1:7001': { vnodes: 50 },
+  '127.0.0.1:7002': { vnodes: 50 },
+  '127.0.0.1:7003': { vnodes: 50 }
+})
+
+const multi = sharding.getMulti()
+await multi.set('a', 'foo')
+await multi.set('b', 'bar')
+await multi.get('a')
+await multi.get('b')
+const replies = await multi.execAsync()
+console.log(replies) // => [127.0.0.1:7002, 127.0.0.1:7000, 'foo', 'bar']
 ```
--- test/multi.spec.js
+++ test/multi.spec.js
@@ -7,13 +7,20 @@ const sharding = new Sharding({
   '127.0.0.1:7003': { vnodes: 50 }
 })
 
-/** need support this stuff */
+// 注意：不支持链式的操作，因为每次 multi 操作可能会按当前的 key 去异步获取一个 server
+const test_exec = async () => {
+  const multi = sharding.getMulti()
+  await multi.set('a', 'foo') // => 7002
+  await multi.set('b', 'bar') // => 7000
+  await multi.get('a')
+  await multi.get('b')
+  return multi.execAsync()
+}
 
-// const multi = sharding.getMulti()
-
-// multi
-//   .set('a', 'foo') // => 7002
-//   .set('b', 'bar') // => 7000
-//   .get('a')
-//   .execAsync()
-//   .then(reply => console.log(reply))
+test_exec().then(replies => {
+  assert.ok(replies[0] === '127.0.0.1:7002', '结果不一致')
+  assert.ok(replies[1] === '127.0.0.1:7000', '结果不一致')
+  assert.ok(replies[2] === 'foo', '结果不一致')
+  assert.ok(replies[3] === 'bar', '结果不一致')
+  process.exit(0)
+})
